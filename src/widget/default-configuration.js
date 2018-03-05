8009e294158719958c38a6327a8d9798c8cb1b83
Marcel Pociot <m.pociot@gmail.com>
2018-03-06 00:17:49 +0100

    Change spacing

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -6,39 +6,39 @@ export const defaultConfiguration = {
 
     /*
      * The location of your chat frame URL / route.
-	 */
+     */
     frameEndpoint: 'chat.html',
 
     /*
      * Date and time format to use
-	 */
+     */
     timeFormat: 'HH:MM',
     dateTimeFormat: 'm/d/yy HH:MM',
 
     /*
      * The title to use in the widget header.
-	 */
+     */
     title: 'BotMan Widget',
 
     /*
      * How many days is the cookie valid?
-	 */
+     */
     cookieValidInDays: 1,
 
     /*
      * This is a welcome message that every new user sees
      * when the widget is opened for the first time.
-	 */
+     */
     introMessage: '',
 
     /*
      * Input placeholder text
-	 */
+     */
     placeholderText: 'Send a message...',
 
     /*
      * Toggle display of message times
-	 */
+     */
     displayMessageTime: true,
 
     mainColor: '#196eb4',
