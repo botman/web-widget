2188c4d36c1eae9d686193cfe3a09eec4831a5e6
Mohsin Hayat <l164333@lhr.nu.edu.pk>
2018-08-13 17:57:31 +0500

    Update State using setState()

--- src/chat/messages/messagetype.tsx
+++ src/chat/messages/messagetype.tsx
@@ -19,8 +19,9 @@ export default abstract class MessageType extends Component<IMessageTypeProps, I
      */
     componentDidMount() {
         setTimeout(() => {
-            this.state.visible = true;
-            this.state.visibilityChanged = true;
+            this.setState({ visible : true });
+            //this.state.visibilityChanged = true;
+            this.setState({ visibilityChanged: true });
             this.onVisibilityChange();
             this.props.onVisibilityChange(this.props.message, this.state);
         }, this.props.timeout || 0);
